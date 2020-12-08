f7cea15124f9859e21ceddbf2ce10031bf048afc
blurfx <its.blureffect@gmail.com>
2020-12-09 01:38:45 +0900

    feat: integrate google analytics

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -18,6 +18,17 @@ const Layout = (props: LayoutProps) => {
       <Head>
         <title>BBConf 2020 Winter</title>
         <meta name='viewport' content='width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, viewport-fit=cover' />
+        <script async src='https://www.googletagmanager.com/gtag/js?id=G-YQPEJXK3F0' />
+        <script dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-YQPEJXK3F0');
+        `,
+        }}
+        />
       </Head>
       {children}
     </>
