fe450f43d118f9e0eca883ce3f97ae1a590f61cd
blurfx <iam@xo.dev>
2022-07-10 23:57:16 +0900

    chore: add opengraph data

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,8 +2,8 @@ import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `conference`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: `BBConf`,
+    siteUrl: `https://bbconf.kr`
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,5 +1,5 @@
 import React, { type PropsWithChildren } from 'react';
-
+import Helmet from 'react-helmet';
 import { globalStyles } from '../../stitches.config';
 import {Container} from './styles';
 import Header from '../Header';
@@ -7,10 +7,20 @@ import Header from '../Header';
 const Index = ({ children }: PropsWithChildren) => {
   globalStyles();
 
-  return <Container>
-    <Header />
-    { children }
-  </Container>;
+  return (
+    <Container>
+      <Helmet>
+        <title>BBConf 2022</title>
+        <meta name="description" content="백준푸는방 컨퍼런스" />
+        <meta property="og:url" content="https://bbconf.kr/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="BBConf" />
+        <meta property="og:description" content="백준푸는방 컨퍼런스" />
+      </Helmet>
+      <Header />
+      { children }
+    </Container>
+  );
 }
 
 export default Index;
