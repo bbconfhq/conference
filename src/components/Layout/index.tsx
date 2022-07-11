834d52595f8b464e3b0d18d0bb7d62d661c66b57
Ryo <9062624+gwanryo@users.noreply.github.com>
2022-07-11 20:25:08 +0900

    feat: add favicon, og thumbnail

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -16,6 +16,12 @@ const Index = ({ children }: PropsWithChildren) => {
         <meta property="og:type" content="website" />
         <meta property="og:title" content="BBConf" />
         <meta property="og:description" content="백준푸는방 컨퍼런스" />
+        <meta property="og:image" content="/thumbnail.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+        <link rel="manifest" href="/favicon/site.webmanifest"></link>
+        <link rel="shortcut icon" href="/favicon/favicon.ico" type="image/x-icon"></link>
       </Helmet>
       <Header />
       { children }
