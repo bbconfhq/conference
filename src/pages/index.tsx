ca77be70356039f44ca5ba8b412ae28b86b71fc9
Ryo <9062624+gwanryo@users.noreply.github.com>
2023-02-13 23:21:27 +0900

    feat: update for 2023-1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,8 +9,8 @@ const IndexPage = () => {
   return (
     <Layout>
       <Title>BBConf 2023</Title>
-      <Time dateTime={'2023-02-26'}>2023년 2월 26일</Time>
-      <Location>토즈 강남토즈타워점 <a href="https://map.naver.com/v5/entry/place/21660996">[지도]</a></Location>
+      <Time dateTime={'2023-02-26 13:00:00'}>2023년 2월 26일 13시</Time>
+      <Location>토즈 강남토즈타워점 <a href="https://map.naver.com/v5/entry/place/21660996">[N]</a> <a href="https://place.map.kakao.com/17206019">[K]</a></Location>
 
       <Divider />
 
