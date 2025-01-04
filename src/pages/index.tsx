7a55553d9a7e234448784df677f8a3efafd109d3
blurfx <iam@xo.dev>
2025-01-04 19:18:06 +0900

    update 2024 winter sessions

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,14 +4,13 @@ import { Title, Time, Location, SessionTitle, Divider } from '../common.styles';
 import Layout from '../components/Layout';
 import SessionList from '../components/SessionList';
 import { useFetchSession } from '../hooks/useFetchSession';
-import {SessionPlaceholder} from '../components/SessionPlaceholder';
 
 const IndexPage = () => {
   const sessions = useFetchSession().allSessionsJson.nodes;
   return (
     <Layout>
       <Title>BBConf 2024 Winter</Title>
-      <Time>2024년 1월 11일 토요일 12:00 ~ 18:00</Time>
+      <Time>2024년 1월 11일 토요일 11:00 ~ 18:00</Time>
       <Location>
         <a href='https://naver.me/Fn2s6XYI' target='_blank' rel='noreferrer'>
           Open UP Playground
@@ -22,7 +21,7 @@ const IndexPage = () => {
 
       <SessionTitle>Sessions</SessionTitle>
       <SessionList sessions={sessions} />
-      <SessionPlaceholder>- TBA -</SessionPlaceholder>
+      {/* <SessionPlaceholder>- TBA -</SessionPlaceholder> */}
     </Layout>
   );
 };
