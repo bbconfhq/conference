6ccac3201c350c3a14f086312dbc98f7502e59c5
Ryo <9062624+gwanryo@users.noreply.github.com>
2025-06-29 22:35:09 +0900

    fix: coming soon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,18 +9,18 @@ const IndexPage = () => {
   const sessions = useFetchSession().allSessionsJson.nodes;
   return (
     <Layout>
-      <Title>BBConf 2024 Winter</Title>
-      <Time>2025년 1월 11일 토요일 11:00 ~ 18:00</Time>
-      <Location>
+      <Title>BBConf 2025</Title>
+      <Time>Coming Soon...</Time>
+      {/* <Location>
         <a href='https://naver.me/Fn2s6XYI' target='_blank' rel='noreferrer'>
           Open UP Playground
         </a>
-      </Location>
+      </Location> */}
 
       <Divider />
 
-      <SessionTitle>Sessions</SessionTitle>
-      <SessionList sessions={sessions} />
+      {/* <SessionTitle>Sessions</SessionTitle> */}
+      {/* <SessionList sessions={sessions} /> */}
       {/* <SessionPlaceholder>- TBA -</SessionPlaceholder> */}
     </Layout>
   );
